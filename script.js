bc35bcdf76581b1693a1f1281f2cdf9b610020d0
Glitch (glitch-hello-website) <none>
2022-09-12 13:48:38 +0000

    🌛🎩 Checkpoint ./script.js:42900803/5785

--- script.js
+++ script.js
@@ -17,13 +17,33 @@ document.addEventListener('DOMContentLoaded', () =>
   }));
 
 
+
+gsap.from('.heading', {y:-500, delay:.2, duration:1, opacity:0, ease:'power4.out'})
+gsap.from('.textOne', {y:500, delay:2.5, duration:2, opacity:0, ease:'power4.out'})
+gsap.from('.textTwo', {y:200, delay:2.5, duration:2, opacity:0, ease:'power4.out'})
+gsap.from('.button-85', {y:50, delay:4, duration:2, opacity:0, ease:'power4.out'})
+gsap.from('.nav', {y:-50, delay:5, duration:1, opacity:0, ease:'power4.out'})
+gsap.from('.par', {y:100, delay:6, duration:2, opacity:0, ease:'power4.out'})
+gsap.to('.heading', {scale:1.1, delay:.1, duration:3, ease:'power4.out'})
+gsap.to('.nextPage', {scale:1.1, delay:1.2, duration:2,  repeat:-1})
+gsap.to(".keyAnimation", {
+    text: "Klyueva",
+    duration: 1,
+    repeat: -1,
+    repeatDelay: .5,
+    ease: "power1.in",
+    yoyo: true,
+})
+
+
+
 particlesJS("particles-js", 
     {"particles":{
         "number":{
-            "value":10,
+            "value":6,
             "density":{
                 "enable":true,
-                "value_area":1000
+                "value_area":800
             }
         },
         "color":{
@@ -51,12 +71,12 @@ particlesJS("particles-js",
                 }
             },
             "size":{
-                "value":10,
+                "value":60,
                 "random":false,
                 "anim":{
                     "enable":true,
-                    "speed":5,
-                    "size_min":40,
+                    "speed":6,
+                    "size_min":20,
                     "sync":false
                 }
             },
@@ -84,24 +104,6 @@ particlesJS("particles-js",
     },
     "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
-
-
-gsap.from('.heading', {y:-500, delay:.2, duration:1, opacity:0, ease:'power4.out'})
-gsap.from('.textOne', {y:500, delay:2.5, duration:2, opacity:0, ease:'power4.out'})
-gsap.from('.textTwo', {y:200, delay:2.5, duration:2, opacity:0, ease:'power4.out'})
-gsap.from('.button-85', {y:50, delay:4, duration:2, opacity:0, ease:'power4.out'})
-gsap.from('.nav', {y:-50, delay:5, duration:1, opacity:0, ease:'power4.out'})
-gsap.from('.par', {y:100, delay:6, duration:2, opacity:0, ease:'power4.out'})
-gsap.to('.heading', {scale:1.1, delay:.1, duration:3, ease:'power4.out'})
-gsap.to('.nextPage', {scale:1.1, delay:1.2, duration:2,  repeat:-1})
-gsap.to(".keyAnimation", {
-    text: "Klyueva",
-    duration: 1,
-    repeat: -1,
-    repeatDelay: .5,
-    ease: "power1.in",
-    yoyo: true,
-})
 
 
 
