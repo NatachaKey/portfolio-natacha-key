0a1650c743bee5496d084312ddeb34723a5c8b79
Glitch (glitch-hello-website) <none>
2022-09-12 13:36:44 +0000

    🌼📡 Checkpoint ./script.js:42900803/4312

--- script.js
+++ script.js
@@ -17,119 +17,73 @@ document.addEventListener('DOMContentLoaded', () =>
   }));
 
 
-particlesJS("particles-js",{
-"particles":{
-    "number":{
-        "value":63,
-        "density":{
-            "enable":true,
-            "value_area":800
-        }
-    },
-    "color":{
-            "value":"#fff"
-    },
-    "shape":{
-        "type":"polygon",
-        "stroke":{
-            "width":0,
-            "color":"#000000"
-        },
-        "polygon":{
-            "nb_sides":5
-        },
-    "image":{
-        "src":"img/github.svg",
-        "width":50,
-        "height":50
-        }
-    },
-    "opacity":{
-        "value":0.5,
-        "random":true,
-        "anim":{
-            "enable":false,
-            "speed":3,
-            "opacity_min":0.1,
-            "sync":false
+particlesJS("particles-js", 
+    {"particles":{
+        "number":{
+            "value":10,
+            "density":{
+                "enable":true,
+                "value_area":1000
             }
-    },
-    "size":{
-        "value":7,
-        "random":true,
-        "anim":{
-            "enable":false,
-            "speed":40,
-            "size_min":0.1,
-            "sync":false
+        },
+        "color":{
+            "value":"#d1d5f0"
+        },
+        "shape":{
+            "type":"polygon",
+            "stroke":{
+                "width":0,
+                "color":"#000"
+            },
+            "polygon":{
+                "nb_sides":6
+            },
+            "image":{
+                "src":"img/github.svg","width":100,"height":100}
+            },"opacity":{
+                "value":0.3,
+                "random":true,
+                "anim":{
+                    "enable":false,
+                    "speed":1,
+                    "opacity_min":0.1,
+                    "sync":false
+                }
+            },
+            "size":{
+                "value":10,
+                "random":false,
+                "anim":{
+                    "enable":true,
+                    "speed":5,
+                    "size_min":40,
+                    "sync":false
+                }
+            },
+            "line_linked":{
+                "enable":false,
+                "distance":200,
+                "color":"#ffffff",
+                "opacity":1,
+                "width":2
+            },
+            "move":{
+                "enable":true,
+            "speed":5,
+            "direction":"none",
+            "random":false,
+            "straight":false,
+            "out_mode":"out",
+            "bounce":false,
+            "attract":{
+                "enable":false,
+                "rotateX":600,
+                "rotateY":1200
+            }
         }
     },
+    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
-    "line_linked":{
-        "enable":false,
-        "distance":500,
-        "color":"#ffffff",
-        "opacity":0.4,
-        "width":2
-    },
-    "move":{
-        "enable":true,
-        "speed":3,
-        "direction":"top-right",
-        "random":true,
-        "straight":true,
-        "out_mode":"out",
-        "bounce":true,
-        "attract":{
-            "enable":true,
-            "rotateX":1200,
-            "rotateY":1200}
-}
-    },
-
-"interactivity":{
-"detect_on":
-    "canvas",
-        "events":{
-            "onhover":{
-                "enable":true,
-                "mode":"bubble"
-            },
-            "onclick":{
-                "enable":true,
-                "mode":"repulse"
-            },
-            "resize":true
-        },
-    
-    "modes":{
-        "grab":{
-            "distance":400,
-            "line_linked":{
-            "opacity":0.5}
-        },
-        "bubble":{
-            "distance":400,
-            "size":4,
-            "duration":0.3,
-            "opacity":1,
-            "speed":1
-        },
-        "repulse":{
-            "distance":300,
-            "duration":1
-        },
-            "push":{
-            "particles_nb":20
-        },
-    "remove":{
-        "particles_nb":2
-    }
-    }
-},
-
-"retina_detect":true
-    });
 
 
 gsap.from('.heading', {y:-500, delay:.2, duration:1, opacity:0, ease:'power4.out'})
